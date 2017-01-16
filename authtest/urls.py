007b18e15a86767b3f56fb1be2b221faeb724247
Richard Giddings <rgiddings20@hotmail.com>
2017-01-16 10:01:14 +0000

    Add basic registration example and link to other possible functionality.

--- authtest/urls.py
+++ authtest/urls.py
@@ -7,9 +7,25 @@ from django.contrib import admin
 from django.contrib.auth import views
 from log.forms import LoginForm
 
+# Registration
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('log.urls')),
+
+    # Registration functionality
+    url('^register/', CreateView.as_view(
+            template_name='register.html',
+            form_class=UserCreationForm,
+            success_url='/login'
+    ), name='register'),
+    
+    # login/lgout
     url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
-    url(r'^logout/$', views.logout, {'next_page': '/login'}),  
+    url(r'^logout/$', views.logout, {'next_page': '/login'}), 
+
+    # More functionality can be added using views from
+    # https://github.com/django/django/blob/master/django/contrib/auth/urls.py
 ]
